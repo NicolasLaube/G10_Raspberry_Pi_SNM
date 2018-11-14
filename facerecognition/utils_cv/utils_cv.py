f756ec05125fa1e0cb15a8bea83014f41d283512
Melvin <melvin.bicho@student-cs.fr>
2018-11-14 15:33:39 +0100

    Ceci ne marche pas

--- facerecognition/utils_cv/utils_cv.py
+++ facerecognition/utils_cv/utils_cv.py
@@ -4,11 +4,13 @@
 # Melvin BICHO
 # package utilitaire pour le projet
 #####################################
+'''
 import cv2
 import matplotlib.pyplot as plt
 import os
 import numpy as np
-
+'''
+'''
 Filepath = "../Data/tetris_blocks.png"
 
 
@@ -57,3 +59,52 @@ def process_image_2(filepath):
 
 
 process_image_2(Filepath)
+'''
+'''
+face_cascade = cv2.CascadeClassifier('C:/Users/melvi/Coding Weeks/G10_Raspberry_Pi_SNM/facerecognition/data/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('C:/Users/melvi/Coding Weeks/G10_Raspberry_Pi_SNM/facerecognition/data/haarcascade_eye.xml')
+img = cv2.imread('C:/Users/melvi/Coding Weeks/G10_Raspberry_Pi_SNM/facerecognition/data/melvin.bicho5.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+plt.imshow(img)
+plt.show()
+
+'''
+import numpy as np
+import cv2 as cv
+
+def reco_webcam():
+
+   face_cascade = cv.CascadeClassifier('E:/Coding/xml/haarcascade_frontalface_default.xml')
+   eye_cascade = cv.CascadeClassifier('E:/Coding/xml/haarcascade_eye.xml')
+
+#Webcam capture
+   video_capture = cv.VideoCapture(0)
+   while True :
+       ret, frame = video_capture.read()
+       frame = cv.flip(frame, 1)
+
+       gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+       faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+
+       for (x,y,w,h) in faces:
+           cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+           roi_gray = gray[y:y+h, x:x+w]
+           roi_color = frame[y:y+h, x:x+w]
+           eyes = eye_cascade.detectMultiScale(roi_gray)
+           for (ex,ey,ew,eh) in eyes:
+               cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+       cv.imshow('img',frame)
+       if cv.waitKey(1) == 27:
+           break  # esc to quit
+   cv.destroyAllWindows()
