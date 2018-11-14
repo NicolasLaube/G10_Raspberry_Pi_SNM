249bbbc350fb3b46dcf8e8b5543db8b110796b41
Melvin <melvin.bicho@student-cs.fr>
2018-11-14 10:35:17 +0100

    .

--- facerecognition/utils_cv/utils_cv.py
+++ facerecognition/utils_cv/utils_cv.py
@@ -5,6 +5,7 @@
 # package utilitaire pour le projet
 #####################################
 import cv2
+import matplotlib.pyplot as plt
 
 
 def load_and_display_image(filepath):
@@ -12,14 +13,12 @@ def load_and_display_image(filepath):
 
     img = cv2.imread(filepath, 1)
     if img is not None:
-        cv2.imshow('img', img)
-
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        plot = plt.imshow(img)
+        plt.show()
     else:
         raise ValueError("L'image" + filepath + "n'existe pas")
 
 
-load_and_display_image("C:/Users/Nicolas LAUBE/G10_Raspberry_Pi_SNM/facerecognition/Data/tetris_blocks.png")
+load_and_display_image("C:/Users/melvi/Coding Weeks/G10_Raspberry_Pi_SNM/facerecognition/data/tetris_blocks.png")
 
 
