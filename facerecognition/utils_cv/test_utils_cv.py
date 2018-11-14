3943830531f3d49f86605140fd96c2c0aa360753
Nicolas_LAUBE <nicolas.laube@student-cs.fr>
2018-11-14 10:56:51 +0100

    modifs print et FFFFilepath

---
+++ facerecognition/utils_cv/test_utils_cv.py
@@ -0,0 +1,14 @@
+
+import unittest
+from utils_cv.py import *
+
+class Utils_test(unittest.TestCase):
+
+    #def setUp(self):
+
+    def test_tetris_blocks(self):
+        self.assertTrue(load_and_display_image("C:/Users/Nicolas LAUBE/G10_Raspberry_Pi_SNM/facerecognition/Data/tetris_blocks.png") )
+
+
+if __name__ == '__main__':
+    unittest.main()
