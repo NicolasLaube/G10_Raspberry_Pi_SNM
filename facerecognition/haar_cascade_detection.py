554620f7737cc8efd77df401dd3304fe1b65745c
Nicolas_LAUBE <nicolas.laube@student-cs.fr>
2018-11-16 09:19:36 +0100

    Fonctionnalité qui fonctionne avec les parses et liste image

--- facerecognition/haar_cascade_detection.py
+++ facerecognition/haar_cascade_detection.py
@@ -31,21 +31,63 @@ def reco_webcam():
     cv.destroyAllWindows()
 
 
-face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
-img = cv.imread('Data/melvin.bicho3.jpg')
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-cv.imshow('img',img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+def face_delimitation(facepath):
+    face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+    eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
+    img = cv.imread(facepath)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    cv.imshow('img',img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
+#face_delimitation(facepath='Data/melvin.bicho3.jpg')
 #reco_webcam()
 
+
+# import the necessary packages
+from imutils import paths
+import face_recognition
+import argparse
+import pickle
+import cv2
+import os
+
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--dataset", required=True, help="path to input directory of faces + images")
+ap.add_argument("-e", "--encodings", required=True, help="path to serialized db of facial encodings")
+ap.add_argument("-d", "--detection-method", type=str, default="cnn", help="face detection model to use: either `hog` or `cnn`")
+args = vars(ap.parse_args())
+
+
+print("[INFO] quantifying faces...")
+imagePaths = list(paths.list_images(args["dataset"]))
+
+# initialize the list of known encodings and known names
+knownEncodings = []
+knownNames = []
+
+for (i, imagePath) in enumerate(imagePaths):
+    # extract the person name from the image path
+    print("[INFO] processing image {}/{}".format(i + 1, len(imagePaths)))
+    name = imagePath.split(os.path.sep)[-2]
+
+    # load the input image and convert it from BGR (OpenCV ordering)
+    # to dlib ordering (RGB)
+    image = cv2.imread(imagePath)
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+
+
+
+if __name__ == "main":
+    main()
