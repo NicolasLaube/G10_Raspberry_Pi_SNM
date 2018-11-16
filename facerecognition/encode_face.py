e9b04be09f0b4cf05210bc4d074ca5f541d4a5a7
Nicolas_LAUBE <nicolas.laube@student-cs.fr>
2018-11-16 13:17:24 +0100

    Fonctionnalité video et image reconnaissance des visages

---
+++ facerecognition/encode_face.py
@@ -0,0 +1,63 @@
+#face_delimitation(facepath='Data/melvin.bicho3.jpg')
+#reco_webcam()
+
+
+# import the necessary packages
+from imutils import paths
+import face_recognition
+import argparse
+import pickle
+import cv2
+import os
+
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--dataset", required=True, help="path to input directory of faces + images")
+ap.add_argument("-e", "--encodings", required=True, help="path to serialized db of facial encodings")
+ap.add_argument("-d", "--detection-method", type=str, default="cnn", help="face detection model to use: either `hog` or `cnn`")
+args = vars(ap.parse_args())
+
+
+print("[INFO] quantifying faces...")
+imagePaths = list(paths.list_images(args["dataset"]))
+
+# initialize the list of known encodings and known names
+knownEncodings = []
+knownNames = []
+
+for (i, imagePath) in enumerate(imagePaths):
+    # extract the person name from the image path
+    print("[INFO] processing image {}/{}".format(i + 1, len(imagePaths)))
+    name = imagePath.split(os.path.sep)[-1][:-5]
+    print(name)
+
+    # load the input image and convert it from BGR (OpenCV ordering)
+    # to dlib ordering (RGB)
+    image = cv2.imread(imagePath)
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    boxes = face_recognition.face_locations(rgb, model=args["detection_method"])
+
+    # compute the facial embedding for the face
+    encodings = face_recognition.face_encodings(rgb, boxes)
+
+    # loop over the encodings
+    for encoding in encodings:
+        # add each encoding + name to our set of known names and
+        # encodings
+        knownEncodings.append(encoding)
+        knownNames.append(name)
+
+
+# dump the facial encodings + names to disk
+print("[INFO] serializing encodings...")
+data = {"encodings": knownEncodings, "names": knownNames}
+print(data)
+f = open(args["encodings"], "wb")
+f.write(pickle.dumps(data))
+f.close()
+
+
+if __name__ == "main":
+    main()
